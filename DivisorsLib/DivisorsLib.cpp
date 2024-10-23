9afb4c511584a2f2367a600bbbe75b8335d82dc2
Aidin26a <alimseitovaydin@gmail.com>
2024-10-23 19:14:25 +0200

    faster algorythm

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,13 +1,21 @@
 #include <vector>
+#include <cmath>  // for std::sqrt
 #include "Divisors.h"
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
-	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
-	{
-		if (number % i == 0)
-			result.push_back(i);
-	}
-	return result;
+    std::vector<unsigned long long> result;
+    unsigned long long limit = static_cast<unsigned long long>(std::sqrt(number));
+
+    for (unsigned long long i = 1; i <= limit; ++i)
+    {
+        if (number % i == 0)
+        {
+            result.push_back(i);  // i is a divisor
+            if (i != number / i)
+                result.push_back(number / i);  // number / i is also a divisor
+        }
+    }
+
+    return result;
 }
